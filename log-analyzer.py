1f879f676268d42deb5aeab5b29ac676d8767012
Arham Jarif <arhamjarif007@gmail.com>
2026-07-03 10:08:03 +0600

    initial commit for stage 2

--- log-analyzer.py
+++ log-analyzer.py
@@ -1,5 +1,26 @@
 # timestamp|event|IP address|user
-# dict for most active user/ip
+
+'''
+========== SUMMARY ==========
+
+Total events
+Failed logins
+Successful logins
+Deleted files
+Most active user
+Most active IP
+
+========== SECURITY ALERTS ==========
+
+Possible brute-force attack
+Repeated failed logins from same IP
+Repeated failed logins for same user
+Successful login after multiple failures (user)
+Multiple accounts targeted (IP)
+Mass deletion of files in small time (<5 min,user)
+Repeated password changes (3 times in 1 day,user)
+Login of an account from 2 different IPs <1 min
+'''
 try:
     with open("log.txt") as file:
         total_events = 0
@@ -17,20 +38,28 @@ try:
                 successful_login += 1
             elif "FILE_DELETED" in fields[1]:
                 deleted_file += 1
+
+
             if fields[3] in users_activity:
                 users_activity[fields[3]] += 1
-            elif fields[3] not in users_activity:
+            else:
                 users_activity[fields[3]] = 1
+
+
             if fields[2] in ips_activity:
                 ips_activity[fields[2]] += 1
-            elif fields[2] not in ips_activity:
+            else:
                 ips_activity[fields[2]] = 1
+
+
         highest_user_activity = 0
         most_active_user = 'temp'
         for key,value in users_activity.items():
             if value > highest_user_activity:
                 most_active_user = key
                 highest_user_activity = value
+
+
         highest_ip_activity = 0
         most_active_ip = 'temp'
         for key,value in ips_activity.items():
@@ -41,7 +70,7 @@ try:
 
 
         print('========== SUMMARY ==========')
-        print(f"Total events:{total_events}\nFailed logins:{failed_login}\nSuccessful logins:{successful_login}\nDeleted files:{deleted_file}\nMost active user:{most_active_user}({highest_user_activity} logins)\nMost active IP:{most_active_ip} ({highest_ip_activity} logins)")
+        print(f"Total events:{total_events}\nFailed logins:{failed_login}\nSuccessful logins:{successful_login}\nDeleted files:{deleted_file}\nMost active user:{most_active_user}({highest_user_activity} events)\nMost active IP:{most_active_ip} ({highest_ip_activity} events)")
         
 except FileNotFoundError:
     print("Log not found")
